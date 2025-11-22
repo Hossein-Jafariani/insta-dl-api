dd33fbe5f1c930bece2713f3724857003b5fdeed
hossein jafariani <hossein.jafarian.90@gmail.com>
2025-11-23 00:22:10 +0330

    Update start.sh with shebang and comments
    Added shebang and comments in Persian for clarity.

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
-pip install flask gunicorn requests
+#!/usr/bin/env bash
+
+# نصب همه پکیج‌های مورد نیاز برای ۳ لایه
+pip install flask gunicorn requests instaloader
+# آپدیت yt-dlp
 pip install --upgrade --force-reinstall "git+https://github.com/yt-dlp/yt-dlp.git"
+
+# اجرا
 gunicorn --workers 4 app:app
