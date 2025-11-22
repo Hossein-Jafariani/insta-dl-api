cf7d8f8fea74f602cefe7612739f5909203208e4
hossein jafariani <hossein.jafarian.90@gmail.com>
2025-11-22 23:56:23 +0330

    Remove unnecessary comments from start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
-#!/usr/bin/env bash
-
-# نصب پکیج‌ها (requests اضافه شد)
 pip install flask gunicorn requests
-# نصب آخرین نسخه yt-dlp
 pip install --upgrade --force-reinstall "git+https://github.com/yt-dlp/yt-dlp.git"
-
-# اجرای برنامه
 gunicorn --workers 4 app:app
