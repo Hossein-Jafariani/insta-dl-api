d30e26db7176a25ff1541a01ac47fba72e31b522
hossein jafariani <hossein.jafarian.90@gmail.com>
2025-11-24 21:11:48 +0330

    Remove unnecessary lines from start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
-#!/usr/bin/env bash
-
-# نصب همه پکیج‌های مورد نیاز برای ۳ لایه
 pip install flask gunicorn requests instaloader
-# آپدیت yt-dlp
 pip install --upgrade --force-reinstall "git+https://github.com/yt-dlp/yt-dlp.git"
-
-# اجرا
 gunicorn --workers 4 app:app
